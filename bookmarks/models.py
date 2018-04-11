e9704ee7b2de1d75f01c7a445cf482c4af043962
William Antony <william@pathadan.com>
2018-04-11 13:31:13 -0500

    Set up /bookmarks route of Bookmarks

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,12 @@
+from uuid import uuid4
 from django.db import models
 
 # Create your models here.
+class Bookmark(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  name = models.CharField(max_length=200)
+  notes = models.TextField('Notes', blank=True)
+  url = models.URLField('URL', unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  last_modified = models.DateTimeField(auto_now=True)
+
