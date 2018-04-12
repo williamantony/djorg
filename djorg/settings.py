1b3fb555d3cfb39c8df96c8bd452f8a5c0bf89f5
William Antony <william@pathadan.com>
2018-04-12 17:26:02 -0500

    Setup static and created styles.css

--- djorg/settings.py
+++ djorg/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/william/Lambda/djorg/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
