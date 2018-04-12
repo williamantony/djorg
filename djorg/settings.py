432f5617821bc2f6d8e9bd20c5390a932097ad87
William Antony <william@pathadan.com>
2018-04-12 12:16:36 -0500

    Added Bootstrap

--- djorg/settings.py
+++ djorg/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party Applications
+    'bootstrap4',
     # Our Applications
     'bookmarks',
 ]
