77090c0100384d3a8cf59cf8f009a60e6f0809dc
William Antony <william@pathadan.com>
2018-04-12 13:04:23 -0500

    Setup / route to display default view

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html')),
     path('bookmarks/', include('bookmarks.urls')),
     path('admin/', admin.site.urls),
 ]
