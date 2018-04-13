dffc1a925a05095e4de2e417d2ae1913eab405df
William Antony <william@pathadan.com>
2018-04-13 01:09:19 -0500

    Changed the placeholder of Input fields

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -6,3 +6,8 @@ class BookmarkForm(forms.ModelForm):
   class Meta:
     model = Bookmark
     fields = ( 'name', 'url', 'notes' )
+    widgets = {
+        'name': forms.TextInput(attrs={'placeholder': 'e.g. Example Website'}),
+        'url': forms.TextInput(attrs={'placeholder': 'e.g. https://www.example.com'}),
+        'notes': forms.Textarea(attrs={'placeholder': ''}),
+    }
