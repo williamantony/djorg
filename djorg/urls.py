0e3ee1df9b938bce5d63220d0a2d7ee8075a540c
William Antony <william@pathadan.com>
2018-04-17 23:52:17 -0500

    Added graphql route in urls.py

--- djorg/urls.py
+++ djorg/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from graphene_django.views import GraphQLView
 from rest_framework import routers
 from notes.api import NoteViewSet
+
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'notes', NoteViewSet)
@@ -27,6 +30,7 @@ router.register(r'notes', NoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('graphql/', GraphQLView.as_view(graphiql = True)),
     path('bookmarks/', include('bookmarks.urls')),
-    path('', TemplateView.as_view(template_name='base.html')),
+    path('', views.index, name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
