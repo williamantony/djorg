3850c7c6cee09000dcf0b8a22b82b4be069ea761
William Antony <william@pathadan.com>
2018-04-25 22:29:21 -0500

    Added anonymous-redirection in homepage view

--- djorg/views.py
+++ djorg/views.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  print('\n\nIndex in djorg\n\n')
-  return render(request, 'base.html')
+  if request.user.is_anonymous:
+    return HttpResponseRedirect(settings.LOGIN_URL)    
+  return render(request, 'djorg_base.html')
