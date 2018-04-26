61211c445a99eef9bfa297c88c250a8e67efd745
William Antony <william@pathadan.com>
2018-04-25 22:29:09 -0500

    Added anonymous-redirection in files view

--- files/views.py
+++ files/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import FileForm
@@ -5,6 +7,8 @@ from .models import File
 
 # Create your views here.
 def index(request):
+  if request.user.is_anonymous:
+    return HttpResponseRedirect(settings.LOGIN_URL)
   if request.method == 'POST':
     form = FileForm(request.POST, request.FILES)
     if form.is_valid():
